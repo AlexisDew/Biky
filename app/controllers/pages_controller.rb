6e5b3f53700d353bbf7c6b4a601dfcd9cd8d8c07
AlexisDew <alexis.dewerdt@gmail.com>
2019-06-19 18:37:52 +0200

    acces aux politiques de conf sans connexion

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :cgu, :confidentialite]
 
   def home
+  end
+
+  def cgu
+  end
+
+  def confidentialite
   end
 end
